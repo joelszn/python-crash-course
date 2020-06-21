4c29d385d753269173444299039738dabe4a773e
Joel Duran <duranjoel.a@gmail.com>
2020-06-21 13:37:19 -0400

    Just getting started

---
+++ 3-chapter/3-1-Names.py
@@ -0,0 +1,10 @@
+names = ['roman','davion','leslie']
+
+print(names[:])
+
+# 3-2 Greetings
+print(f'{names[0]} is lit')
+print(f'{names[1]} is lit')
+print(f'{names[2]} is lit')
+
+# 3-3 didn't serve much purpose
